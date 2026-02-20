ddd0467bc1059445f9df4f74c302b524c5be679d
vitor <vitor@DESKTOP-F4SNSMO>
2026-02-20 10:20:02 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Models/Produtos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace MauiAppMinhasCompras.Models
+{
+    public class Produtos
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Quantidade { get; set; }
+        public double Preco { get; set; }
+    }
+}
